6359e711458c3c326600f9759e0eb30167702838
tushar <tushars@centillionsoftech.com>
2024-02-16 12:13:32 +0530

    changes

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const routes = require("./src/routes");
 const path = require("path");
 const server = express();
 const cors = require("cors");
+server.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 require("dotenv").config({ path: ".env" });
 console.log("process.env.FILE_PATH", process.env.FILE_PATH);
 process.env.FILE =
@@ -13,11 +18,6 @@ console.log("process.env.FILE", process.env.FILE);
 server.use(express.json());
 server.use("/api/v1", routes);
 server.use("/static", express.static(__dirname + "/public"));
-server.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  })
-);
 
 server.listen(9000, () => {
   console.log("server is calling in 9000 port");
